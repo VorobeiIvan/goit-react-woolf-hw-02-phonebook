f567ac84a247639fac36c131c3ecc2330e81f11b
Ivan <0675361905vi@gmail.com>
2024-02-07 13:40:26 +0200

    add Section

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import App from 'components/App/App';
 import '../css/min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
